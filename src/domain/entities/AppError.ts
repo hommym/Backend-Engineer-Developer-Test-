0fd77f1c7c8328c8266354eec02cd20d4c5629f0
herberth arthur <herbertharthur80@gmail.com>
2024-09-06 15:39:27 +0000

    Created custom error class for throw explicit errors

---
+++ src/domain/entities/AppError.ts
@@ -0,0 +1,13 @@
+
+
+
+export class AppError extends Error{
+    message:string;
+    statusCode:number;
+
+    constructor(message:string,statusCode:number){
+        super(message)
+        this.message=message
+        this.statusCode=statusCode
+    }
+}
